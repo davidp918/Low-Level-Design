2ec20f85daca850f0f216904f54936efdff2d204
David <dp1220@ic.ac.uk>
2024-10-08 15:12:13 +0100

    restructure

--- solutions/golang/coffeeVendingMachine/interfaces/coffee.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-import "github.com/ashishps1/awesome-low-level-design/coffeeVendingMachine/enums"
-
-type Coffee interface {
-	Cost() int
-	Recipe() map[enums.Ingredient]int
-	Name() string
-}
--- solutions/golang/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ashishps1/awesome-low-level-design
-
-go 1.23.1
